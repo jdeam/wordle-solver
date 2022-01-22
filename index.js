126e4b01e13f8dd7a5ff3c677be0a4016ca22db9
Jordan Deam <jordan.deam@gmail.com>
2022-01-22 13:41:43 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const playwright = require('playwright');
+const { getNextGuess } = require('./utils');
+
+const main = async () => {
+    const browser = await playwright.chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    await context.grantPermissions(["clipboard-read", "clipboard-write"]);
+
+    const page = await context.newPage();
+    await page.goto('https://www.powerlanguage.co.uk/wordle/');
+    await page.click('.close-icon');
+
+    const hits = new Set();
+    const toExclude = []
+    const toIncludeNotAt = [];
+    const toIncludeAt = [];
+
+    let answer = '';
+
+    for (let i = 0; i < 6; i++) {
+        const guess = getNextGuess({
+            unique: !i,
+            toExclude,
+            toIncludeNotAt,
+            toIncludeAt,
+        });
+
+        await page.type('#game', guess);
+        await page.keyboard.press('Enter');
+        await page.waitForTimeout(2000);
+
+        const rows = await page.$$('game-row');
+        const tiles = await rows[i].$$('game-tile');
+        const results = await Promise.all(tiles.map(tile => tile.getAttribute('evaluation')));
+        
+        if (results.every(r => r === 'correct')) {
+            answer = guess;
+            break;
+        }
+
+        for (let i = 0; i < 5; i++) { 
+            const l = guess[i];
+
+            switch (results[i]) {
+                case 'absent': 
+                    if (!hits.has(l)) toExclude.push(l);
+                    break;
+                case 'present': 
+                    toIncludeNotAt.push([i, l]);
+                    hits.add(l);
+                    break;
+                case 'correct':
+                    toIncludeAt.push([i, l]);
+                    hits.add(l);
+                    break;
+            }
+        }
+    }
+
+    await page.click('#share-button');
+    const squares = await page.evaluate(() => navigator.clipboard.readText());
+    console.log(`Answer: ${answer}\n\n${squares}`);
+
+    await browser.close();
+};
+
+main();
---
+++ utils.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+
+const wordList = fs.readFileSync('sgb-words.txt')
+    .toString()
+    .split('\n');
+
+const positionToLetterCount = [];
+
+wordList.forEach(word => {    
+    word.split('').forEach((l, i) => {
+        const letterToCount = positionToLetterCount[i] || {};
+        const count = letterToCount[l];
+        if (count) letterToCount[l]++;
+        else letterToCount[l] = 1;
+        positionToLetterCount[i] = letterToCount;
+    });
+});
+
+const getWordValue = (word) => {
+    return word.split('').reduce((v, l, i) => {
+        return v + positionToLetterCount[i][l];
+    }, 0); 
+};
+
+const hasAllUniqueLetters = (word) => {
+    return new Set(word.split('')).size === 5;
+};
+
+const excludes = (letters) => {
+    return (word) => {
+        return word
+            .split('')
+            .every(letter => !letters.includes(letter));
+    };
+};
+
+const includesNotAt = (indicesWithLetters) => {
+    return (word) => {
+       return indicesWithLetters.every(([index, letter]) => {
+           return word[index] !== letter && word.includes(letter);
+        });
+    };
+};
+
+const includesAt = (indicesWithLetters) => {
+    return (word) => {
+        return indicesWithLetters
+            .every(([index, letter]) => word[index] === letter);
+    };
+};
+
+const getNextGuess = ({
+    unique,
+    toExclude,
+    toIncludeNotAt,
+    toIncludeAt,
+}) => {    
+    const conditions = [
+        unique && hasAllUniqueLetters,
+        excludes(toExclude),
+        includesNotAt(toIncludeNotAt),
+        includesAt(toIncludeAt),
+    ].filter(Boolean);
+
+    const sortedOptions = wordList
+        .reduce((acc, word) => {
+            const shouldInclude = conditions.every(condition => condition(word));
+            if (shouldInclude) acc.push({word, val: getWordValue(word)});
+            return acc;
+        }, [])
+        .sort((a, b) => b.val - a.val);
+    
+    return sortedOptions[0]?.word;
+};
+
+module.exports = { getNextGuess };
