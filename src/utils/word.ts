935dc85fb3527c595afdb81f6fe03778cd6c5bea
Chris Spalding <cspalding93@gmail.com>
2022-01-29 20:38:25 -0800

    add ability to score words and save result to json file

--- src/utils/word.ts
+++ src/utils/word.ts
@@ -1,6 +1,8 @@
-import fs from 'fs';
+import fs from "fs";
+import path from "path";
 
-const wordList: string[] = fs.readFileSync('src/words_long.txt')
+const wordList: string[] = fs
+    .readFileSync('data/words_long.txt')
     .toString()
     .split(/\r?\n|\r/);
 
@@ -19,7 +21,7 @@ export const getResults = (
             copy[i] = null;
         }
     });
-    // Then mark present/absent letters ...
+    // Then mark present/absent letters
     guess.split('').forEach((l, i) => {
         if (results[i]) return;
         if (copy.includes(l)) {
@@ -53,7 +55,7 @@ const wordListWithVals: WordWithVal[] = wordList
 type Validator = (word: string) => boolean;
 
 const hasAllUniqueLetters: Validator = (word) => {
-    return new Set(word.split('')).size === 5;
+  return new Set(word.split("")).size === 5;
 };
 
 const includes = (toInclude: Set<string>): Validator => {
@@ -103,4 +105,39 @@ export const getNextGuess = (
         }, null);
 
     return word;
+};
+
+/**
+ * Given a filename with a newline separated list of words, calculate each word's 'score' and return the resulting
+ * Object with each word as a key and its score as the value.
+ * @param wordListFilename string name of the file that contains words we want to score.
+ * @param outFilename string name of the file where the output should be saved
+ * @param wordScore (x: string) => number the function to use to score each word
+ * @returns Object<string, number> that maps a word to its score
+ */
+ export const saveWordScores = (
+    wordListFilename: string = 'words_long.txt',
+    outFilename: string = 'words_long_scores.json',
+    wordScore: (word: string) => number = getWordVal,
+  ): { [word: string]: number } => {
+    // if caller only provided a filename without a path (i.e. output.json) then
+    // default to looking up the input file in the 'data' directory of this project
+    if (!wordListFilename.includes('/')) {
+      wordListFilename = path.resolve(__dirname, `../../data/${wordListFilename}`);
+    }
+    const words = fs.readFileSync(wordListFilename).toString().split(/\r?\n|\r/);
+    const result = {};
+    words.forEach((word) => {
+      result[word] = wordScore(word);
+    });
+  
+    // if caller only provided a filename without a path (i.e. output.json) then
+    // default to storing the resulting file in the 'data' directory of this project
+    if (!outFilename.includes('/')) {
+      outFilename = path.resolve(__dirname, `../../data/${outFilename}`);
+    }
+    const stringResult = JSON.stringify(result, null, 4);
+    fs.writeFileSync(outFilename, stringResult);
+  
+    return result;
 };
